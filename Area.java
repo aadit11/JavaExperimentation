3134e7499aa04d559b4e0e334bb4e9f000f865ac
aadit11 <aadit.kshirsagar@gmail.com>
2025-01-07 20:27:15 +0530

    Added two more prog

---
+++ Area.java
@@ -0,0 +1,22 @@
+package JavaExperimentation;
+public class Area
+
+{
+    public static void main(String[] args)
+    
+    {
+        double radius = 2.5;
+        
+        double PI = 3.14;
+        
+        
+       double Area = PI * radius * radius;
+        
+        System.out.println("Area of circle is : " + Area);
+        
+        
+    }
+    
+}    
+        
+      
