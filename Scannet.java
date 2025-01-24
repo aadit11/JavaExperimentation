77474f6618c957c81119baf166ae961bac66a0dc
aadit11 <aadit.kshirsagar@gmail.com>
2025-01-24 09:52:09 +0530

    Added a file

---
+++ Scannet.java
@@ -0,0 +1,15 @@
+public class Scannet
+{
+    public static void main(String[] args)
+    {
+        int length = 234;
+        int breadth = 786;
+        int height = 529;
+        
+        int volume = length * breadth * height;
+        
+        System.out.println("The volume of the object is : " + volume);
+    }
+}    
+        
+        
