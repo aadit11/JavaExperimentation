f95ddbc03aaf1e62c55d5937dc4a005efa7d57b1
aadit11 <aadit.kshirsagar@gmail.com>
2025-01-28 08:39:21 +0530

    Added a file

---
+++ SumAndProduct.java
@@ -0,0 +1,20 @@
+public class SumAndProduct
+{
+    public static void main(String[] args)
+    {
+        int  number1 = 15;
+        int number2 = 12;
+        
+        int sum = number1 + number2;
+        
+        int product = number1 * number2;
+        
+        System.out.println("First Number is : " + number1);
+        System.out.println("Second Number is : " + number2);
+        System.out.println("Sum of Number1 and Number2 is : " + sum);
+        System.out.println("Product of Number1 and Number2 is : " + product);
+        
+    }
+    
+}
+        
