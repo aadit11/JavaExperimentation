3dbd6e0a20ef2ddd0624ee3e754e29bd5659ff89
aadit11 <aadit.kshirsagar@gmail.com>
2025-01-30 08:42:22 +0530

    Added a file

---
+++ Volume.java
@@ -0,0 +1,15 @@
+public class Volume
+{
+    public void main(String[] args)
+    {
+        int length = 254;
+        int breadth = 987;
+        int height = 564;
+        
+        int volume = length * breadth * height;
+        
+        System.out.println("The volume of the cuboid is : " + volume);
+    }
+}    
+        
+   
